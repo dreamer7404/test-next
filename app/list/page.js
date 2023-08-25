1039730117d97c4e97d81982e78b30c536b4be92
ahnks <dreamer7404@daum.net>
2023-08-25 23:52:29 +0900

    first commit

---
+++ app/list/page.js
@@ -0,0 +1,21 @@
+
+'use client'
+
+export default function List() {
+    let goods = ['tomato', 'pasta', 'coconut']
+
+    // console.log(goods)
+
+    return (
+      <div >
+        <h4 onClick={()=>{}} className="title">상품목록</h4>
+        <div className="food">
+            <h4>상품1 $40</h4>
+        </div>
+        <div className="food">
+            <h4>상품2 $40</h4>
+        </div>
+      </div>
+    )
+  }
+  
